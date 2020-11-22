ad08a5f6cea5106eafa3efbc6e4b1882f7eb58e7
Anakonda11 <Xapollon430@gmail.com>
2020-11-22 16:51:50 -0500

    Updates

---
+++ precache-manifest.d01d7e6eb1843a5ab47b2c22d384aa2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f2e541ef8813c6d3f3f",
+    "url": "/PurpleHeart/static/css/main.9e3c888a.chunk.css"
+  },
+  {
+    "revision": "3f2e541ef8813c6d3f3f",
+    "url": "/PurpleHeart/static/js/main.7bca4ae9.chunk.js"
+  },
+  {
+    "revision": "63f2e2753a750e7bcc89",
+    "url": "/PurpleHeart/static/js/runtime~main.c9cb06cb.js"
+  },
+  {
+    "revision": "4026d71470c58d227a05",
+    "url": "/PurpleHeart/static/js/2.f0df25e8.chunk.js"
+  },
+  {
+    "revision": "148bd775f1a3aaa4efed7465de0b9898",
+    "url": "/PurpleHeart/index.html"
+  }
+];
